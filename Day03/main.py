b4d1ac01b37c660a1a3edbdf561a54726521a153
David <davidcoates95@gmail.com>
2025-12-03 20:21:14 +1100

    day3 simpler

--- Day03/main.py
+++ Day03/main.py
@@ -1,26 +1,17 @@
-import functools
-
 
 def read_input():
     with open("input.txt") as file:
         return [ [ int(chr) for chr in line.rstrip('\n') ] for line in file.readlines() ]
 
-def concat(*xs: int) -> int:
-    return int(''.join(str(x) for x in xs))
+def concat(x: int, y: int) -> int:
+    return int(f"{x}{y}")
 
-def max_joltage(bank: list[int], m: int) -> int:
-
-    @functools.lru_cache(maxsize=None)
-    def max_joltage_(i: int, m: int):
-        if m == 1:
-            return max(bank[i:])
-        elif m == len(bank) - i:
-            return concat(*bank[i:])
-        else:
-            return max(concat(bank[i], max_joltage_(i+1, m-1)), max_joltage_(i+1, m))
-
-    return max_joltage_(0, m)
-
+def max_joltage(bank: list[int], n: int) -> int:
+    if n == 1:
+        return max(bank)
+    else:
+        i = max(range(len(bank) - (n - 1)), key=lambda i: bank[i])
+        return concat(bank[i], max_joltage(bank[i+1:], n - 1))
 
 def part1():
     print(sum(max_joltage(bank, 2) for bank in read_input()))
